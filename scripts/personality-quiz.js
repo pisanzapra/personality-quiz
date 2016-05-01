f9dd8618fdea639170e17424a11dcf7ee2a034d7
pisanzapra <pisanzapra@users.noreply.github.com>
2016-05-01 10:01:54 +0100

    Various improvements incl bg image, styling, results presentation

--- scripts/personality-quiz.js
+++ scripts/personality-quiz.js
@@ -1,9 +1,16 @@
 "use strict";
 
-//creating the object type: Question
+// creating the object type: Question
 function Question(questionText, answers) {
     this.questionText = questionText;
     this.answers = answers;
+}
+
+// creating the object type: Result
+function Result(title, image, description) {
+    this.title = title;
+    this.image = image;
+    this.description = description;
 }
 
 var questionWrapper = document.getElementById("question-wrapper");
@@ -19,15 +26,21 @@ var scoreB = 0;
 var scoreC = 0;
 var scoreD = 0;
 
-// store user answers in an array - might use this for customising feedback
-// var userAnswers = new Array(); 
+var userAnswers = new Array();
 
 // creating an Array of Questions
 var allTheQuestions = new Array();
-allTheQuestions.push(new Question("Personality quiz question 1", ["Q1 Answer A", "Q1 Answer B", "Q1 Answer C", "Q1 Answer D"]));
+allTheQuestions.push(new Question("The text for the question will appear here. This is where the text for the question will appear.", ["Q1 Answer A", "Q1 Answer B", "Q1 Answer C", "Q1 Answer D"]));
 allTheQuestions.push(new Question("Personality quiz question 2", ["Q2 Answer A", "Q2 Answer B", "Q2 Answer C", "Q2 Answer D"]));
 allTheQuestions.push(new Question("Personality quiz question 3", ["Q3 Answer A", "Q3 Answer B", "Q3 Answer C", "Q3 Answer D"]));
 allTheQuestions.push(new Question("Personality quiz question 4", ["Q4 Answer A", "Q4 Answer B", "Q4 Answer C", "Q4 Answer D"]));
+
+// creating an Array of Results
+var allTheResults = new Array();
+allTheResults.push(new Result("Personality A", "image-a.jpg", "Description of Personality A here"));
+allTheResults.push(new Result("Personality B", "image-b.jpg", "Description of Personality B here"));
+allTheResults.push(new Result("Personality C", "image-c.jpg", "Description of Personality C here"));
+allTheResults.push(new Result("Personality D", "image-d.jpg", "Description of Personality D here"));
 
 // making the current Question appear on the page, ensuring no answers are pre-selected
 function setQuestion(questionNumber) {
@@ -57,7 +70,7 @@ function setQuestion(questionNumber) {
         quizRadioLabel.className = 'quiz-label';
         quizRadioLabel.htmlFor = 'answer' + i;
         quizRadioLabel.id = 'answer' + i + 'label';
-        quizRadioLabel.innerText = questionAllAnswers[i - 1];
+        quizRadioLabel.innerHTML = questionAllAnswers[i - 1];
 
         answerDiv.appendChild(quizRadio);
         answerDiv.appendChild(quizRadioLabel);
@@ -65,31 +78,9 @@ function setQuestion(questionNumber) {
 
     }
 
-    // if we're on any Question other than Question 1, show the start again button 
-    if (currentQuestion >= allTheQuestions.length - 4) {
-
-        while (startAgainWrapper.hasChildNodes()) {
-            startAgainWrapper.removeChild(startAgainWrapper.lastChild);
-        }
-
-        var startAgainButton = document.createElement("input");
-        startAgainButton.type = 'button';
-        startAgainButton.className = 'quiz-button';
-        startAgainButton.id = 'start-again-button'
-        startAgainButton.value = "Start again \u203a\u203a"
-        startAgainButton.onclick = function () {
-            userAnswers.length = 0;
-            score = 0;
-            displayNewQuestion(0);
-        };
-
-        startAgainWrapper.appendChild(startAgainButton);
-
-    }
-
     // if we're on the last question, change the next button text
     if (currentQuestion === allTheQuestions.length - 1) {
-        document.getElementById("next-button").value = "Show me the results";
+        document.getElementById("next-button").value = "Show me my result!";
     }
 }
 
@@ -106,8 +97,6 @@ function checkAnswer() {
     var answerBselected = document.getElementById('answer2').checked;
     var answerCselected = document.getElementById('answer3').checked;
     var answerDselected = document.getElementById('answer4').checked;
-
-    // userAnswers.push(correctAnswerSelected); keep for customising results text later
 
     if (answerAselected) {
         scoreA++;
@@ -126,64 +115,45 @@ function checkAnswer() {
     }
 }
 
+
+
+function revealPersonality(largestType) {
+    feedback.innerHTML = "Your dominant personality type is " + largestType.title;
+    var resultImage = document.createElement('div');
+    resultImage.classname = 'result-image';
+    resultImage.innerHTML = "<img src='" + largestType.image + "' />";
+    feedback.appendChild(resultImage);
+    var resultDescription = document.createElement('div');
+    resultDescription.classname = 'result-description';
+    resultDescription.innerHTML = largestType.description;
+    feedback.appendChild(resultDescription);
+}
+
+
 // revealing the results
 function showResults() {
 
     questionWrapper.innerHTML = " ";
 
     var largest = Math.max(scoreA, scoreB, scoreC, scoreD);
+
     if (scoreA == largest) {
-        feedback.innerHTML = "A score:" + scoreA + "<br />" + "B score:" + scoreB + "<br />" + "C score:" + scoreC + "<br />" + "D score:" + scoreD + "<br />" + "Your dominant personality type is A";
+        revealPersonality(allTheResults[0]);
     }
 
     else if (scoreB == largest) {
-        feedback.innerHTML = "A score:" + scoreA + "<br />" + "B score:" + scoreB + "<br />" + "C score:" + scoreC + "<br />" + "D score:" + scoreD + "<br />" + "Your dominant personality type is B";
+        revealPersonality(allTheResults[1]);
     }
     else if (scoreC == largest) {
-        feedback.innerHTML = "A score:" + scoreA + "<br />" + "B score:" + scoreB + "<br />" + "C score:" + scoreC + "<br />" + "D score:" + scoreD + "<br />" + "Your dominant personality type is C";
+        revealPersonality(allTheResults[2]);
     }
 
     else if (scoreD == largest) {
-        feedback.innerHTML = "A score:" + scoreA + "<br />" + "B score:" + scoreB + "<br />" + "C score:" + scoreC + "<br />" + "D score:" + scoreD + "<br />" + "Your dominant personality type is D";
+        revealPersonality(allTheResults[3]);
     }
     else {
-        feedback.innerHTML = "A score:" + scoreA + "<br />" + "B score:" + scoreB + "<br />" + "C score:" + scoreC + "<br />" + "D score:" + scoreD + "<br />" + "Something else happened";
+        feedback.innerHTML = "Oops! Something went wrong there";
     }
-
-    /*
-    questionWrapper.innerHTML = " ";
-    feedback.innerHTML = "You scored " + ((score / allTheQuestions.length) * 100) + "%";
-
-    for (var i = 0; i < allTheQuestions.length; i++) {
-
-        var response = document.createElement('div');
-        response.className = 'response';
-        response.innerText = 'You answered question ' + (i + 1) + ' ';
-
-        var answer_span = document.createElement('span');
-
-        if (userAnswers[i]) {
-            answer_span.className = 'quiz-correct-answer';
-            answer_span.innerText = 'correctly';
-            response.appendChild(answer_span);
-            response.appendChild(document.createTextNode('.'));
-        }
-        else {
-            var correctAnswerValue = allTheQuestions[i].correctAnswer - 1;
-            var correctAnswerText = allTheQuestions[i].answers[correctAnswerValue];
-            answer_span.className = 'quiz-incorrect-answer';
-            answer_span.innerText = 'incorrectly';
-            response.appendChild(answer_span);
-            response.appendChild(document.createTextNode('.'))
-            var correctAnswerReveal = document.createTextNode(' The correct answer is ' + correctAnswerText);
-            response.appendChild(correctAnswerReveal);
-        }
-
-        feedback.appendChild(response);
-
-        
-    }
-*/
 }
 
 function hasUserSelectedAnAnswer() {
@@ -227,3 +197,41 @@ function nextQuestion() {
 
 
 displayNewQuestion(0);
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1722679911277589',
+        xfbml: true,
+        version: 'v2.6'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+} (document, 'script', 'facebook-jssdk'));
+
+
+function feedDialog(resultName, resultDescription, resultCaption, resultImage) {
+    FB.ui(
+        {
+            method: 'feed',
+            name: resultName,
+            link: window.location.href,
+            description: resultDescription,
+            caption: resultCaption,
+            picture: resultImage,
+            /*display: 'popup'*/
+        },
+        function (response, show_error) {
+            if (response && response.post_id) {
+                console.log('Post was published.');
+            } else {
+                console.log('Post was not published.');
+            }
+        }
+    );
+}
